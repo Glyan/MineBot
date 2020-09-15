c4ef49e5b94c0824bbf56be5abb81ee4f0a2b1b5
Glyan <pamela27jr@gmail.com>
2020-09-15 18:26:27 +0100

    Added dependencies and check for voice channel and server

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json')
+const { prefix, token } = require('./config.json');
+const ytdl = require("ytdl-core");
 
 const client = new Discord.Client();
+var servers = {};
 
 client.once('ready', () => {
     console.log('Ready!')
 })
 
 client.on('message', message => {
-    if (message.content.startsWith(`${prefix}random`))
-        message.channel.send("Random playing");
+    
+    let args = message.content.substring(prefix.length).split(" ");
+
+    switch (args[0]) {
+        case 'play':
+            if (!args[1]) {
+                message.channel.send("There is no link!");
+                return;
+            }
+
+            if (!message.member.voiceChannel) {
+                message.channel.send("You must be in a channel to play the bot!")
+                return;
+            }
+
+            if (!servers[message.guild.id]) servers[message.guild.id] = {
+                queue: []
+            }
+            
+        break;
+    }
 })
 
 client.login(token);
