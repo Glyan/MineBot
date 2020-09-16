31c4aafc1bed623d75e2893e14c900c66194ed4f
Glyan <pamela27jr@gmail.com>
2020-09-16 18:22:34 +0100

    Added play function

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ client.on('message', message => {
 
     switch (args[0]) {
         case 'play':
+
+            function play (connection, message) {
+                var server = servers[message.guild.id];
+                server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}))
+                server.queue.shift();
+                server.dispatcher.on("end", function(){
+                    if (server.queue[0]) {
+                        play(connection, message);
+                    } else {
+                        connection.disconnect();
+                    }
+                })
+            }
+
             if (!args[1]) {
                 message.channel.send("There is no link!");
                 return;
@@ -28,6 +42,13 @@ client.on('message', message => {
             if (!servers[message.guild.id]) servers[message.guild.id] = {
                 queue: []
             }
+
+            var server = servers[message.guild.id];
+
+            if (!message.guild.voiceConnection) 
+                message.member.voiceChannel.join().then(function(connection) {
+                    play(connection, message);
+                })
             
         break;
     }
